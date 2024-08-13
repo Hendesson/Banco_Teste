76110fbe72e20cadb51dead11d60706196698761
Hendesson <hendesson124@gmail.com>
2024-08-13 15:33:12 -0300

    Fim do curso

--- Main.py
+++ Main.py
@@ -1,13 +1,13 @@
 class Main:
     pass
 
-print("Testando")
-
 from Cliente import Cliente
 from Conta import Conta
 
 c1= Cliente("João", "11444-2222")
-conta=Conta(c1.nome,6565,0)
+conta=Conta(c1.get_nome(), 1222)
 
 
-print(conta.titular, "Numero: ",conta.numero, "Seu Saldo: ", conta.saldo)
+conta.deposita(100)
+conta.saque(50)
+conta.extrato()
